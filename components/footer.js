96161bd7cf1aead3623015b5340915c656711eb0
khaledsellami <35802509+khaledsellami@users.noreply.github.com>
2025-01-29 13:36:12 -0500

    update header and footer with proper links

--- components/footer.js
+++ components/footer.js
@@ -10,8 +10,8 @@ class Footer extends HTMLElement {
         <div class="py-3 px-md-5 container-fluid">
             <div class="row">
                 <div class="col-auto col-md-5 align-middle">
-                    <p class="mb-0" style="color: var(--light-gray);">© 2024 Your Name</p>
-                    <p class="my-0 pt-0"><a class="" href="mailto:yourname@email.org">yourname@email.org</a></p>
+                    <p class="mb-0" style="color: var(--light-gray);">© 2025 Khaled Sellami</p>
+                    <p class="my-0 pt-0"><a class="" href="mailto:khaled.sellami.1@ulaval.ca">khaled.sellami.1@ulaval.ca</a></p>
 
                 </div>
 
@@ -19,11 +19,11 @@ class Footer extends HTMLElement {
                 <div class="col col-md-7 text-right">
                     <p class="mb-0" style="color: var(--light-gray);">Quick Links</p>
                     <p class="my-0 pt-0">
-                        <a href=""><i class="mr-3 footer-icons fa-solid fa-house" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 footer-icons fa-brands fa-google-scholar" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-github" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="footer-icons fa-brands fa-x-twitter" aria-hidden="true"></i></a>
+                        <a href="/index.html"><i class="mr-3 footer-icons fa-solid fa-house" aria-hidden="true"></i></a>
+                        <a href="https://www.linkedin.com/in/khaled-sellami/" target="_blank"><i class="mr-3 fab footer-icons fa-linkedin" aria-hidden="true"></i></a>
+                        <a href="https://github.com/khaledsellami" target="_blank"><i class="mr-3 fab footer-icons fa-github" aria-hidden="true"></i></a>
+                        <a href="https://scholar.google.ca/citations?user=oUZl5aUAAAAJ&hl=en&oi=ao" target="_blank"><i class="mr-3 footer-icons fa-brands fa-google-scholar" aria-hidden="true"></i></a>
+                        <a href="https://orcid.org/0000-0002-6595-2489" target="_blank"><i class="footer-icons fa-brands fa-orcid" aria-hidden="true"></i></a>
                     </p>
                 </div>
             </div>
